f44836e1b554d469d8e12e3734ea0f7e72b4cbcc
Quentin <dsquentin@hotmail.fr>
2022-05-27 00:04:06 +0200

    Starting moovements

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ class Sprite {
    update(){
      this.draw();
      this.position.y += this.velocity.y;
+     this.position.x += this.velocity.x;
 
      if (this.position.y + this.height + this.velocity.y >= canvas.height){
         this.velocity.y = 0;
@@ -53,6 +54,16 @@ const ennemy = new Sprite({
   }
 });
 
+const keys = {
+  q: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
+
+let lastKey
 
 // Permet d'effectuer l'animation en boucle tant que la fenêtre est ouverte
 function animate(){
@@ -61,10 +72,41 @@ function animate(){
   c.fillRect(0,0,canvas.width,canvas.height);
   player.update();
   ennemy.update();
+
+  player.velocity.x = 0;
+  if (keys.q.pressed && lastKey === 'q'){
+    player.velocity.x = -1
+  } else if (keys.d.pressed && lastKey === 'd') {
+    player.velocity.x = 1
+  }
 }
 
 animate();
 
 window.addEventListener('keydown', (e) => {
+  switch(e.key){
+    case 'd':
+      keys.d.pressed = true
+      lastKey = 'd'
+      break
+
+      case 'q':
+        keys.q.pressed = true
+        lastKey = 'q'
+        break
+  }
+  console.log(e)
+})
+
+window.addEventListener('keyup', (e) => {
+  switch(e.key){
+    case 'd':
+      keys.d.pressed = false
+      break
+
+      case 'q':
+      keys.q.pressed = false
+      break
+  }
   console.log(e)
 })
